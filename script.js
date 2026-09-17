3855c1744d0d0af87b2ff4e7a1b911c40b688b8f
Ashab Rahman <114132979+Ashabdiu@users.noreply.github.com>
2026-09-17 18:08:42 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const menuToggle = document.querySelector(".menu-toggle");
+
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+    const icon = menuToggle.querySelector("i");
+
+    icon.classList.toggle("fa-bars");
+
+    icon.classList.toggle("fa-xmark");
+
+});
+
+
+const navItems = document.querySelectorAll(".nav-links a");
+
+navItems.forEach((item) => {
+
+    item.addEventListener("click", () => {
+
+        navLinks.classList.remove("active");
+
+        const icon = menuToggle.querySelector("i");
+
+        icon.classList.add("fa-bars");
+
+        icon.classList.remove("fa-xmark");
+
+    });
+
+});
